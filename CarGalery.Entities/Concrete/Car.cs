987f2cf6d572eda689d2e0b2c7405522d2c3fe47
Ali Furkan Çağlayan <afurkancaglayan07@gmail.com>
2021-01-07 13:17:36 +0300

    Entities...

---
+++ CarGalery.Entities/Concrete/Car.cs
@@ -0,0 +1,32 @@
+﻿using CarGalery.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        //en az üç alan - üç farklı tip
+        public int CarId { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string model, decimal price)
+        {
+            Model = model;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Model,-15} {Price,-15}";
+        }
+    }
+
+}
